a23fdd7aae3f3db6b526ac8a78e01f297579b70b
Denys Bazyliak <franciskdelyamon@gmail.com>
2023-12-01 17:37:05 +0100

    feat/refactor: add colors package & refactor words controller & add new store for temporary data

--- controllers/words.js
+++ controllers/words.js
@@ -1,19 +1,4 @@
-const words = [
-    {
-        word_id:'seq443',
-        word:'node',
-        translation:'вузол',
-        definition:'A point at which lines or pathways intersect',
-        link:'https://translate.google.com'
-    },
-    {
-        word_id:'seq444',
-        word:'knot',
-        translation:'*',
-        definition:'A fastening made by tying a piece of string, rope, or something similar.',
-        link:'https://translate.google.com'
-    }
-]
+const words = require('./')
 
 
 // @desc        Get all words
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require('morgan')
 const connectDB = require('./config/db')
+const colors = require('colors')
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -25,11 +26,11 @@ app.use('/app/v1/words/', words)
 
 const PORT = process.env.PORT || 5001;
 
-const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port:${PORT}`));
+const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port:${PORT}`.green.bold));
 
 // Handle unhandled promise rejections 
 process.on('unhandledRejection', (err, promise)=>{
-    console.log(`Error message: ${err.message}`)
+    console.log(`Error message: ${err.message}`.red.italic)
     // Close server and exit process 
     server.close(()=> process.exit(1))
 })
