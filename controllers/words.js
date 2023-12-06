c2e4feb7846f19b7415d31f3a58c9e1be4e56ff3
Denys Bazyliak <franciskdelyamon@gmail.com>
2023-12-06 19:44:16 +0100

    feat: add pagination

--- controllers/words.js
+++ controllers/words.js
@@ -10,7 +10,8 @@ exports.getWords = asyncHandler(async (req, res, next) => {
 
     const reqQuery = { ...req.query }
 
-    const removeFields = ['select', 'sort']
+    const removeFields = ['select', 'sort', 'page', 'limit']
+
 
     removeFields.forEach(param => delete reqQuery[param])
 
@@ -19,6 +20,7 @@ exports.getWords = asyncHandler(async (req, res, next) => {
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
 
     query = Word.find(JSON.parse(queryStr))
+
 
     if (req.query.select) {
         const fields = req.query.select.split(',').join(' ')
@@ -32,11 +34,36 @@ exports.getWords = asyncHandler(async (req, res, next) => {
         query = query.sort('-createdAt')
     }
 
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 25 
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Word.countDocuments()
+
+    query = query.skip(startIndex).limit(limit)
+
+
     const words = await query
+
+    const pagination = {}
+
+    if(endIndex < total){
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+    if(startIndex > 0){
+        pagination.prev ={
+            page: page - 1,
+            limit
+        }
+    }
 
     res.status(200).json({
         success: true,
         count: words.length,
+        pagination,
         data: words,
     });
 })
