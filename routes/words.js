3befab250025f759b0a6a968e88fd0beb25b1282
Denys Bazyliak <franciskdelyamon@gmail.com>
2023-12-13 19:11:48 +0100

    feat-auth-protect: add protect function

--- routes/words.js
+++ routes/words.js
@@ -2,26 +2,29 @@ const express = require('express')
 
 const router = express.Router()
 
-const {getWord, getWords, patchWord, postWord, putWord,  deleteWord, wordPhotoUpload }= require('../controllers/words')
+const { getWord, getWords, patchWord, postWord, putWord, deleteWord, wordPhotoUpload } = require('../controllers/words')
 
-const Word = require('../models/Word')  
+const Word = require('../models/Word')
 const advancedResults = require('../middleware/advancedResults')
 
-router
-.route('/words/:id/photo')
-.put(wordPhotoUpload)
+
+const { protect } = require('../middleware/auth')
 
 router
-.route('/words')
-.get(advancedResults(Word), getWords)
-.post(postWord)
+    .route('/words/:id/photo')
+    .put(protect, wordPhotoUpload)
 
 router
-.route('/words/:id')
-.get(getWord)
-.put(putWord)
-.patch(patchWord)
-.delete(deleteWord)
+    .route('/words')
+    .get(advancedResults(Word), getWords)
+    .post(protect, postWord)
+
+router
+    .route('/words/:id')
+    .get(getWord)
+    .put(protect, putWord)
+    .patch(protect, patchWord)
+    .delete(protect, deleteWord)
 
 
 module.exports = router;
