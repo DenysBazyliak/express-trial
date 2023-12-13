cb665dad2b752e1fd925de40bb182501528415e1
Denys Bazyliak <franciskdelyamon@gmail.com>
2023-12-13 19:53:33 +0100

    feat-me: add getMe function to gain current user

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const router = express.Router()
 
-const { register, login } = require('../controllers/auth')
+const { register, login, getMe } = require('../controllers/auth')
+const { protect } = require('../middleware/auth')
 
 const User = require('../models/User')
 
-const router = express.Router()
+
 
 router
     .post('/auth/register', register)
 
 router
     .post('/auth/login', login)
+
+router
+    .get('/auth/me', protect, getMe)
 
 module.exports = router
